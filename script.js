264bbb48c37b6f14fe3b5af71039bed4cde005c6
chrytory <1md10119w@gmail.com>
2024-08-17 08:46:13 +0900

    changed script

--- script.js
+++ script.js
@@ -130,7 +130,7 @@ document.addEventListener('DOMContentLoaded', () => {
 // サービスワーカーの登録
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', function() {
-        navigator.serviceWorker.register('/service-worker.js').then(function(registration) {
+        navigator.serviceWorker.register('/rgb-qr-reader-pwa/service-worker.js').then(function(registration) {
             console.log('ServiceWorker registration successful with scope: ', registration.scope);
         }, function(err) {
             console.log('ServiceWorker registration failed: ', err);
--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,32 @@
 const CACHE_NAME = 'rgb-qr-reader-cache-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
+  '/rgb-qr-reader-pwa/',
+  '/rgb-qr-reader-pwa/index.html',
+  '/rgb-qr-reader-pwa/style.css',
+  '/rgb-qr-reader-pwa/script.js',
+  '/rgb-qr-reader-pwa/manifest.json',
   'https://docs.opencv.org/4.x/opencv.js'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(function(response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
   );
 });
